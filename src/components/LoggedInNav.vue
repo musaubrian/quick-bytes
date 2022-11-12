6b490cf4328e4b5dbecb78ebfb555464c2db2bde
musaubrian <musaubrian15@gmail.com>
2022-11-12 18:11:38 +0300

    chore: add svg line thickness

--- src/components/LoggedInNav.vue
+++ src/components/LoggedInNav.vue
@@ -13,7 +13,7 @@
                     SignOut
                 </li>
                 <li class="p-2 inline-flex items-center md:hidden" @click="dropDown = !dropDown">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -25,10 +25,10 @@
     </nav>
     <div class="bg-gray-200" v-if="dropDown">
         <ul class="flex flex-col items-center cursor-pointer">
-            <li class="p-2 mt-3 border-2 rounded-md border-orange-400 inline-flex items-center justify-center w-5/12">
+            <li class="p-2 mt-3 border-2 rounded-md border-orange-400 inline-flex items-center justify-center w-6/12">
                 <RouterLink to="/add">Add recipes</RouterLink>
             </li>
-            <li class="p-2 mt-3 mb-3 bg-orange-400 rounded-md inline-flex items-center justify-center w-5/12">
+            <li class="p-2 mt-3 mb-3 bg-orange-400 rounded-md inline-flex items-center justify-center w-6/12">
                 SignOut
             </li>
         </ul>

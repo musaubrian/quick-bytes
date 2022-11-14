590607d001493aba4a60d535a4dcefd338b7efea
musaubrian <musaubrian15@gmail.com>
2022-11-14 13:20:19 +0300

    chore: update floating button

--- src/components/FloatingButton.vue
+++ src/components/FloatingButton.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="w-full fixed bottom-1 mb-5 right-0 md:hidden inline-flex items-end justify-end">
-        <span class="p-3 bg-orange-500 rounded-full cursor-pointer inline-flex items-center mr-3" @click="reload()">
-            <i class="material-icons text-gray-800">sync</i></span>
+        <span class="p-3 bg-orange-500 opacity-90 rounded-full cursor-pointer inline-flex items-center mr-3"
+            @click="reload()">
+            <i class="material-icons text-gray-800">sync</i>
+
+        </span>
+
     </div>
 </template>
 
 <script setup>
-
 const reload = () => {
     window.location.reload()
 }

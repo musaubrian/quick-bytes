6bb5fd00037aab2181595bc7471283b506524583
musaubrian <musaubrian15@gmail.com>
2022-11-14 15:13:46 +0300

    feat: add more buttons for mobile

--- src/components/FloatingButton.vue
+++ src/components/FloatingButton.vue
@@ -1,15 +1,28 @@
 <template>
+    <div class="w-full fixed bottom-0 mb-20 right-0 md:hidden inline-flex items-end flex-col justify-end transition duration-1000 ease-in-out"
+        v-if="toggled">
+        <RouterLink to="/add"
+            class="material-icons text-gray-200 p-3 bg-orange-500 opacity-90 rounded-full cursor-pointer inline-flex items-center mr-3 active:bg-orange-600 mb-3">
+            edit</RouterLink>
+        <span @click="reload"
+            class="material-icons text-gray-100 p-3 bg-orange-500 opacity-90 rounded-full cursor-pointer inline-flex items-center mr-3 active:bg-orange-600">sync</span>
+    </div>
     <div class="w-full fixed bottom-1 mb-5 right-0 md:hidden inline-flex items-end justify-end">
-        <span class="p-3 bg-orange-500 opacity-90 rounded-full cursor-pointer inline-flex items-center mr-3"
-            @click="reload()">
-            <i class="material-icons text-gray-800">sync</i>
-
+        <span @click="toggled = !toggled" v-if="!toggled"
+            class="p-3 bg-orange-500 opacity-90 rounded-full cursor-pointer inline-flex items-center justify-center mr-3 active:bg-orange-600 transition duration-700 ease-in-out">
+            <i class="material-icons text-gray-800 text-center">keyboard_arrow_up</i>
         </span>
-
+        <span @click="toggled = !toggled"
+            class="p-3 bg-orange-500 opacity-90 rounded-full cursor-pointer inline-flex items-center justify-center mr-3 active:bg-orange-600 transition duration-700 ease-in-out"
+            v-if="toggled">
+            <i class="material-icons text-gray-800"> keyboard_arrow_down</i>
+        </span>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+const toggled = ref(false)
 const reload = () => {
     window.location.reload()
 }

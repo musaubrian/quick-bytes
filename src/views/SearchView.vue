7fdf3c768f6e858b52ae19585f87ed1ae0385375
musaubrian <musaubrian15@gmail.com>
2022-11-23 23:07:19 +0300

    refactor: remove dead code in search view

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="hero  w-full bg-clip-text">
+    <div class="hero  w-full object-center">
         <img src="../assets/steak.jpg" alt="noodles" class="w-full h-full object-cover">
     </div>
     <form @submit.prevent="lonePageSearch()" class="w-full flex flex-col  justify-center items-center relative">
         <input type="search" id="search" required ref="autoFocus"
-            class="p-3 rounded-lg w-10/12 md:w-5/12 outline-none focus:ring-2 focus:border-transparent text-lg transition-all bg-gray-50"
+            class="p-3 rounded-lg w-10/12 md:w-5/12 drop-shadow-xl outline-none text-lg transition-all bg-gray-50"
             placeholder="Let's find your meal" v-model="searchString" />
         <button type="submit"
             class="inline-flex items-center justify-center bg-blue-400 p-3 rounded-lg mt-2 capitalize text-gray-100 md:w-1/6 active:bg-blue-600 font-semibold transition-all">search</button>

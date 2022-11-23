323732393c9ec1aa7b95f437bf3a898f32e0031d
musaubrian <musaubrian15@gmail.com>
2022-11-23 22:39:21 +0300

    feat: make the search bar in focus on page load

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -3,7 +3,7 @@
         <img src="../assets/steak.jpg" alt="noodles" class="w-full h-full object-cover">
     </div>
     <form @submit.prevent="lonePageSearch()" class="w-full flex flex-col  justify-center items-center relative">
-        <input type="search" id="search" required
+        <input type="search" id="search" required ref="autoFocus"
             class="p-3 rounded-lg w-10/12 md:w-5/12 outline-none focus:ring-2 focus:border-transparent text-lg transition-all bg-gray-50"
             placeholder="Let's find your meal" v-model="searchString" />
         <button type="submit"
@@ -15,6 +15,7 @@
 import { useSearchStore } from '../stores/searchStore'
 import { useAuthStore } from '../stores/authStore';
 import router from '../router';
+import { nextTick, onMounted, ref } from 'vue';
 export default {
     data() {
         return {
@@ -27,18 +28,24 @@ export default {
             searchStore.searchWord = this.searchString
             router.push('/search/recipes')
         }
-
     },
     setup() {
         const searchStore = useSearchStore();
         const authStore = useAuthStore();
+        const autoFocus = ref(null)
         if (authStore.isLoggedIn === false) {
             router.push('/home')
         }
+        onMounted(() => {
+            nextTick(() => {
+                autoFocus.value.focus();
+            });
+        });
 
         return {
             authStore,
-            searchStore
+            searchStore,
+            autoFocus
         }
 
     }
